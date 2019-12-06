3906314f6e868f531ceb6618602e001fee8901a4
Kim <kim.wai.tang@mybit.nl>
2019-12-06 17:07:14 +0100

    screens

--- src/tasks/containers/inbox-screen.stories.ts
+++ src/tasks/containers/inbox-screen.stories.ts
@@ -1,32 +1,27 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { Store, NgxsModule } from '@ngxs/store';
-import { TasksState, ErrorFromServer } from '../state/task.state';
 import { TaskModule } from '../task.module';
-import {HostDispatchErrorComponent} from './host-dispatch-error.component'
+import { NgxsModule, Store } from '@ngxs/store';
+import { TasksState } from '../state/task.state';
+import { TaskListComponent } from './task-list.component';
+import { InboxScreenComponent } from './inbox-screen.component';
+import { PureInboxScreenComponent } from '../components/pure-inbox-screen.component';
 
-storiesOf('InboxScreen', module)
-.addDecorator(
-  moduleMetadata({
-    declarations: [],
-    imports: [TaskModule, NgxsModule.forRoot([TasksState])],
-    providers: [Store],
-  })
-)
- .add('default', () => {
+storiesOf('InboxScreen', module).addDecorator(
+moduleMetadata({
+  imports: [TaskModule, NgxsModule.forRoot([TasksState])],
+  declarations: [InboxScreenComponent, PureInboxScreenComponent, TaskListComponent],
+  providers: [Store],
+}))
+.add('default', () => {
   return {
     template: `<inbox-screen></inbox-screen>`,
   };
 })
- .add('error', () => {
+.add('error', () => {
   return {
     template: `<pure-inbox-screen [error]="error"></pure-inbox-screen>`,
     props: {
       error: 'Something!',
     },
-  };
-})
- .add('Connected Error', () => {
-  return {
-    component: HostDispatchErrorComponent,
   };
 });
--- src/tasks/task.module.ts
+++ src/tasks/task.module.ts
@@ -5,15 +5,15 @@ import { NgxsModule, Store } from '@ngxs/store';
 import { TaskComponent } from './components/task.component';
 import { PureTaskListComponent } from './components/pure-task-list.component';
 import { TasksState } from './state/task.state';
-import { HostDispatchErrorComponent } from './containers/host-dispatch-error.component';
-
-
+import { InboxScreenComponent } from './containers/inbox-screen.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen.component';
+import { TaskListComponent } from './containers/task-list.component';
 
 @NgModule({
   imports: [CommonModule, NgxsModule.forRoot([TasksState])],
   exports: [ TaskComponent, PureTaskListComponent],
-  declarations: [ TaskComponent, PureTaskListComponent, HostDispatchErrorComponent],
-  providers: [Store],
+  declarations: [ TaskComponent, PureTaskListComponent],
+  providers: [],
 })
 export class TaskModule {}
 
